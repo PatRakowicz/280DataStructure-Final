2cc59c26c8db15e7e594c6fce6ad7375eb5b13a0
Patrick Rakowicz <lividsubset3@gmail.com>
2023-04-22 14:31:09 -0600

    Updated

--- BSTController.h
+++ BSTController.h
@@ -1,141 +1,103 @@
-// BSTController.h
-#ifndef BSTCONTROLLER_H
-#define BSTCONTROLLER_H
+//
+// Created by Patrick on 22/04/2023.
+//
+
+#ifndef INC_280DATASTRUCTURE_FINAL_BSTCONTROLLER_H
+#define INC_280DATASTRUCTURE_FINAL_BSTCONTROLLER_H
 
 #include <iostream>
 #include <string>
 #include <memory>
+#include <utility>
+#include <algorithm>
+#include "invGenerator.h"
 
-struct PalletInfo {
-    int pallet_number;
-    std::string state;
-    std::string city;
-    int weight; // in pounds
+struct Node {
+    Pallet pallet;
+    Node *left;
+    Node *right;
+
+    Node(Pallet p) : pallet(p), left(nullptr), right(nullptr) {}
 };
 
-class BSTNode {
+class BST {
 public:
-    PalletInfo data;
-    std::unique_ptr<BSTNode> left;
-    std::unique_ptr<BSTNode> right;
+    BST() : root(nullptr) {}
 
-    BSTNode(PalletInfo d) : data(d), left(nullptr), right(nullptr) {}
-};
-
-class PalletBST {
-private:
-    std::unique_ptr<BSTNode> root;
-
-    void insert(std::unique_ptr<BSTNode> &current, const PalletInfo &data);
-
-    std::unique_ptr<BSTNode> &search(std::unique_ptr<BSTNode> &current, int pallet_number);
-
-    std::unique_ptr<BSTNode> &findMin(std::unique_ptr<BSTNode> &current);
-
-    void remove(std::unique_ptr<BSTNode> &current, int pallet_number);
-
-    void inOrderTraversal(std::unique_ptr<BSTNode> &current);
-
-public:
-    PalletBST() : root(nullptr) {}
-
-    void insert(const PalletInfo &data);
-
-    bool remove(int pallet_number);
-
-    bool search(int pallet_number);
-
-    void printInOrder();
-};
-
-void PalletBST::insert(std::unique_ptr<BSTNode> &current, const PalletInfo &data) {
-    if (!current) {
-        current = std::make_unique<BSTNode>(data);
-        return;
+    void insert(Pallet p) {
+        root = insertHelper(root, p);
     }
 
-    if (data.pallet_number < current->data.pallet_number) {
-        insert(current->left, data);
-    } else {
-        insert(current->right, data);
-    }
-}
-
-std::unique_ptr<BSTNode> &PalletBST::search(std::unique_ptr<BSTNode> &current, int pallet_number) {
-    if (!current || current->data.pallet_number == pallet_number) {
-        return current;
+    void remove(Pallet p) {
+        root = removeHelper(root, p);
     }
 
-    if (pallet_number < current->data.pallet_number) {
-        return search(current->left, pallet_number);
-    } else {
-        return search(current->right, pallet_number);
-    }
-}
-
-std::unique_ptr<BSTNode> &PalletBST::findMin(std::unique_ptr<BSTNode> &current) {
-    if (!current->left) {
-        return current;
-    }
-    return findMin(current->left);
-}
-
-void PalletBST::remove(std::unique_ptr<BSTNode> &current, int pallet_number) {
-    if (!current) {
-        return;
-    }
-
-    if (pallet_number < current->data.pallet_number) {
-        remove(current->left, pallet_number);
-    } else if (pallet_number > current->data.pallet_number) {
-        remove(current->right, pallet_number);
-    } else {
-        if (!current->left && !current->right) {
-            current.reset();
-        } else if (current->left && !current->right) {
-            current = std::move(current->left);
-        } else if (!current->left && current->right) {
-            current = std::move(current->right);
+    Pallet find(int pallet_number) {
+        Node *node = findHelper(root, pallet_number);
+        if (node == nullptr) {
+            Pallet empty;
+            return empty;
         } else {
-            auto &minNode = findMin(current->right);
-            current->data = minNode->data;
-            remove(current->right, minNode->data.pallet_number);
+            return node->pallet;
         }
     }
-}
 
-void PalletBST::inOrderTraversal(std::unique_ptr<BSTNode> &current) {
-    if (!current) {
-        return;
+private:
+    Node *root;
+
+    Node *insertHelper(Node *node, Pallet p) {
+        if (node == nullptr) {
+            node = new Node(p);
+        } else if (p.pallet_number < node->pallet.pallet_number) {
+            node->left = insertHelper(node->left, p);
+        } else {
+            node->right = insertHelper(node->right, p);
+        }
+        return node;
     }
 
-    inOrderTraversal(current->left);
-    std::cout << "Pallet Number: " << current->data.pallet_number
-              << " State: " << current->data.state
-              << " City: " << current->data.city
-              << " Weight: " << current->data.weight << " lbs" << std::endl;
-    inOrderTraversal(current->right);
-}
-
-inline void PalletBST::insert(const PalletInfo &data) {
-    insert(root, data);
-}
-
-inline bool PalletBST::remove(int pallet_number) {
-    std::unique_ptr<BSTNode> &nodeToDelete = search(root, pallet_number);
-    if (!nodeToDelete) {
-        return false;
+    Node *removeHelper(Node *node, Pallet p) {
+        if (node == nullptr) {
+            return node;
+        } else if (p.pallet_number < node->pallet.pallet_number) {
+            node->left = removeHelper(node->left, p);
+        } else if (p.pallet_number > node->pallet.pallet_number) {
+            node->right = removeHelper(node->right, p);
+        } else {
+            if (node->left == nullptr) {
+                Node *temp = node->right;
+                delete node;
+                return temp;
+            } else if (node->right == nullptr) {
+                Node *temp = node->left;
+                delete node;
+                return temp;
+            } else {
+                Node *temp = minNode(node->right);
+                node->pallet = temp->pallet;
+                node->right = removeHelper(node->right, temp->pallet);
+            }
+        }
+        return node;
     }
-    remove(root, pallet_number);
-    return true;
-}
 
-inline bool PalletBST::search(int pallet_number) {
-    return static_cast<bool>(search(root, pallet_number));
-}
+    Node *findHelper(Node *node, int pallet_number) {
+        if (node == nullptr || node->pallet.pallet_number == pallet_number) {
+            return node;
+        } else if (pallet_number < node->pallet.pallet_number) {
+            return findHelper(node->left, pallet_number);
+        } else {
+            return findHelper(node->right, pallet_number);
+        }
+    }
 
-inline void PalletBST::printInOrder() {
-    inOrderTraversal(root);
-}
+    Node *minNode(Node *node) {
+        Node *current = node;
+        while (current->left != nullptr) {
+            current = current->left;
+        }
+        return current;
+    }
+};
 
-#endif //BSTCONTROLLER_H
+#endif //INC_280DATASTRUCTURE_FINAL_BSTCONTROLLER_H
