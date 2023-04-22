b476a7e549ce901de828d7fb9268fa2e4435e5f6
Patrick Rakowicz <lividsubset3@gmail.com>
2023-04-22 14:49:51 -0600

    Changed to not sure namespace std

--- invGenerator.h
+++ invGenerator.h
@@ -8,31 +8,30 @@
 #include <random>
 #include <chrono>
 
-using namespace std;
 
 // Define a struct to represent a pallet
 struct Pallet {
     int pallet_number;
-    string state;
-    string city;
+    std::string state;
+    std::string city;
     int weight;  // in pounds
     int qoh;
 };
 
 class InventoryGenerator {
 public:
-    InventoryGenerator(const string& filename) {
+    InventoryGenerator(const std::string &filename) {
         // Read the CSV file containing cities and states
-        ifstream infile(filename);
-        string line;
-        string tmp;
+        std::ifstream infile(filename);
+        std::string line;
+        std::string tmp;
         getline(infile, line); // Skip the header line
         while (getline(infile, line)) {
-            string city, state;
-            istringstream iss(line);
+            std::string city, state;
+            std::istringstream iss(line);
             getline(iss, city, ',');
-            getline(iss, tmp,',');
-            getline(iss, tmp,',');
+            getline(iss, tmp, ',');
+            getline(iss, tmp, ',');
             getline(iss, state_name, ',');
             cities.push_back(city);
             states.push_back(state_name);
@@ -42,41 +41,43 @@ public:
     Pallet generate_pallet(int pallet_number) {
         // Check that cities and states vectors are non-empty and have the same size
         if (cities.empty() || cities.size() != states.size()) {
-            throw runtime_error("Invalid state and city data");
+            throw std::runtime_error("Invalid state and city data");
         }
 
         // Generate a random index for the city and state vectors
         // unsigned seed = chrono::system_clock::now().time_since_epoch().count();
         unsigned seed = pallet_number;
-        mt19937 gen(seed);
-        uniform_int_distribution<> index_dist(0, cities.size()-1);
+        std::mt19937 gen(seed);
+        std::uniform_int_distribution<> index_dist(0, cities.size() - 1);
         int index = index_dist(gen);
 
         // Create the pallet and return it
-        Pallet pallet = { pallet_number, states[index], cities[index], generate_weight(seed), generate_qoh(seed) };
+        Pallet pallet = {pallet_number, states[index], cities[index], generate_weight(seed), generate_qoh(seed)};
         return pallet;
     }
 
 private:
-    vector<string> cities;
-    vector<string> states;
-    string state_name; // added for reading state name from CSV
+    std::vector<std::string> cities;
+    std::vector<std::string> states;
+    std::string state_name; // added for reading state name from CSV
 
     int generate_weight(int seed) {
         // Generate a random weight between 1 and 1000 pounds
         // unsigned seed = chrono::system_clock::now().time_since_epoch().count();
 
-        mt19937 gen(seed);
-        uniform_int_distribution<> weight_dist(25, 200);
+        std::mt19937 gen(seed);
+        std::uniform_int_distribution<> weight_dist(25, 200);
         return weight_dist(gen);
     }
+
     int generate_qoh(int seed) {
         // Generate a random weight between 1 and 1000 pounds
         // unsigned seed = chrono::system_clock::now().time_since_epoch().count();
 
-        mt19937 gen(seed);
-        uniform_int_distribution<> qoh_dist(1, 200);
+        std::mt19937 gen(seed);
+        std::uniform_int_distribution<> qoh_dist(1, 200);
         return qoh_dist(gen);
     }
 };
+
 #endif // INVENTORY_GENERATOR_H
