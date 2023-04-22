1adbe608bce9f4cc5e678d9f5841ac440e0e6111
Patrick Rakowicz <lividsubset3@gmail.com>
2023-04-22 14:50:23 -0600

    Updated

--- BSTController.h
+++ BSTController.h
@@ -108,7 +108,8 @@ private:
             return;
         }
         printInorderHelper(node->left);
-        std::cout << "Pallet " << node->pallet.pallet_number << ": " << node->pallet.state << ", " << node->pallet.city << ", " << node->pallet.weight << " pounds, QOH: " << node->pallet.qoh << "\n";
+        std::cout << "Pallet " << node->pallet.pallet_number << ": " << node->pallet.state << ", " << node->pallet.city
+                  << ", " << node->pallet.weight << " pounds, QOH: " << node->pallet.qoh << "\n";
         printInorderHelper(node->right);
     }
 };
