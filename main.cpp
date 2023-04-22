e71d0d03745eec356e5503c8582c2ba54002c3f2
Patrick Rakowicz <lividsubset3@gmail.com>
2023-04-22 14:31:24 -0600

    Added test functions for both BST and InvGen

--- main.cpp
+++ main.cpp
@@ -3,17 +3,47 @@
 #include "BSTController.h"
 #include "invGenerator.h"
 
-int main() {
+using namespace std;
+
+void TestpalletBST() {
     PalletBST palletBST;
 
     PalletInfo pallet1 = {1, "CA", "Los Angeles", 150};
     PalletInfo pallet2 = {3, "NY", "New York", 180};
     PalletInfo pallet3 = {2, "TX", "Dallas", 200};
+    PalletInfo pallet4 = {4, "FL", "Miami", 100};
 
     palletBST.insert(pallet1);
     palletBST.insert(pallet2);
     palletBST.insert(pallet3);
+    palletBST.insert(pallet4);
+
+    cout << "In-order traversal:" << endl;
+    palletBST.printInOrder();
+
+    cout << "\nSearching for pallet number 2: " << (palletBST.search(2) ? "Found" : "Not found") << endl;
+    cout << "Searching for pallet number 5: " << (palletBST.search(5) ? "Found" : "Not found") << endl;
+
+    cout << "\nDeleting pallet number 2" << endl;
+    palletBST.remove(2);
+
+    cout << "\nIn-order traversal after deletion:" << endl;
+    palletBST.printInOrder();
+}
+
+void testInvGen() {
+    const int NUM_PALLETS = 10;
+
+    InventoryGenerator generator("data.csv");
+
+    for (int i = 1; i <= NUM_PALLETS; i++) {
+        Pallet pallet = generator.generate_pallet(i);
+        cout << "Pallet " << pallet.pallet_number << ": " << pallet.state << ", " << pallet.city << ", " << pallet.weight << " pounds\n";
+    }
+}
+
+int main() {
+    testInvGen();
 
     return 0;
 }
-
